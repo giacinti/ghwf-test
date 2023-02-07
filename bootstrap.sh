ed5c317b39168393ae4598cf7b3eac31534a2d69
Philippe Giacinti <philippe@giacinti.com>
2023-02-07 14:52:50 +0100

    test 2

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,5 @@
 #! /bin/sh
 mkdir -p /var/lib/rancher/rke2/server/manifests
 wget -c https://api.preprod.anywhere.navify.com/cilium/install-rke2 -O /var/lib/rancher/rke2/server/manifests/cilium-install.yaml
- curl -fL https://rancher.dev-phi-06.staging.anywhere.navify.com/system-agent-install.sh | sudo  sh -s - --server https://rancher.dev-phi-06.staging.anywhere.navify.com --label 'cattle.io/os=linux' --token sn7mt7rbqphfh5x9q2d4p6hnnpxznx4cjlqr2nr9scdbcn642qkfjc --etcd --controlplane --worker
+ curl -fL https://rancher.dev-phi-06.staging.anywhere.navify.com/system-agent-install.sh | sudo  sh -s - --server https://rancher.dev-phi-06.staging.anywhere.navify.com --label 'cattle.io/os=linux' --token rrh9trkh4ndcdkcg8llpfz2h27hnv8rbm24z658km86tpc24pn466p --etcd --controlplane --worker
+
